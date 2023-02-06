d219a2898d428bb123ca29a8d5ba2276853662f6
emmababy <bomvll@naver.com>
2023-02-06 19:39:21 +0900

    init commit

---
+++ src/main/java/com/zerobase/morse/MorseApplication.java
@@ -0,0 +1,15 @@
+package com.zerobase.morse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorseApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MorseApplication.class, args);
+        System.out.println("Hello World");
+    }
+
+}
