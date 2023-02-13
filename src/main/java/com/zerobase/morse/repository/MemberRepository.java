9501d3927e0fb6f92b63f8e6579713489bf46765
emmababy <bomvll@naver.com>
2023-02-13 18:17:45 +0900

    fix : MemberRepository 에러 수정

---
+++ src/main/java/com/zerobase/morse/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.zerobase.morse.repository;
+
+import com.zerobase.morse.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, String> {
+}
