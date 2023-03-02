328758bdd57db81175629f4b971f1803ce6a880b
jaeseong-kim <doctorwho123@naver.com>
2023-03-02 20:59:53 +0900

    Feat: ApplicantListController, StudyController 추가

--- src/main/java/com/zerobase/morse/controller/StudyController.java
+++ src/main/java/com/zerobase/morse/controller/StudyController.java
@@ -1,5 +1,6 @@
 package com.zerobase.morse.controller;
 
+<<<<<<< HEAD
 import com.zerobase.morse.entity.Study;
 import com.zerobase.morse.model.StudyInput;
 import com.zerobase.morse.model.StudyUpdate;
@@ -77,4 +78,24 @@ public class StudyController {
 		studyService.deleteStudy(id);
 		return ResponseEntity.ok().build();
 	}
+=======
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class StudyController {
+
+  /**
+   * 스터디 리스트를 볼 수 있는 임시용 페이지입니다.
+   * @return 스터디리스트 페이지
+   */
+  @GetMapping("/studylist")
+  public String studyList(){
+    return "studylist";
+  }
+
+
+>>>>>>> 6f90c5b (Feat: ApplicantListController, StudyController 추가)
 }
